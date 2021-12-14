7f45ee3622b4b2bbf150a0998223f24a3308c2f4
jeremias ramirez <jereramirez499@gmail.com>
2021-12-14 16:15:17 -0400

    Add http library and services

---
+++ lib/services/get-contacts.services.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class ContactServices extends StatefulWidget {
+  ContactServicesFull createState() => new ContactServicesFull();
+}
+
+class ContactServicesFull extends State<ContactServices> {
+  Widget build(context) {
+    return Text('Hello services');
+  }
+}
