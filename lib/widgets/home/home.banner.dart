77e0e045dca5da027756b752da0a72135b4cf365
jeremias ramirez <jereramirez499@gmail.com>
2021-12-15 12:47:20 -0400

    fix banner

--- lib/widgets/home/home.banner.dart
+++ lib/widgets/home/home.banner.dart
@@ -4,11 +4,12 @@ class BannerHome {
   AppBar banner(String title) {
     return AppBar(
       backgroundColor: Colors.transparent,
-      leading: Icon(Icons.arrow_back, color: Colors.grey[300], size: 22),
+      leading:
+          Icon(Icons.arrow_back_rounded, color: Colors.grey[300], size: 22),
       centerTitle: true,
       title: Text(title, style: TextStyle(fontSize: 14.0)),
       elevation: 0,
-      toolbarHeight: 35,
+      toolbarHeight: 45,
     );
   }
 }
