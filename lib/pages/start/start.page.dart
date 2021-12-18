9c892395de4a2febc082c88c019fc27026449773
jeremias ramirez <jereramirez499@gmail.com>
2021-12-17 23:50:26 -0400

    pagina start configurada

--- lib/pages/start/start.page.dart
+++ lib/pages/start/start.page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class StartPage extends StatefulWidget {
@@ -7,12 +8,55 @@ class StartPage extends StatefulWidget {
 class StartPageFull extends State<StartPage> {
   List<ListTile> contacts = [];
 
+  Future futureHome() {
+    return Future.delayed(Duration(seconds: 3), () => true);
+  }
+
+  void setHome(BuildContext context) {
+    this.futureHome().then((value) {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    this.setHome(context);
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Start',
         theme: ThemeData.dark(),
-        home: Scaffold(body: ListView(physics: ScrollPhysics(), children: [])));
+        home: Scaffold(
+            body: Center(
+                child: Container(
+                    height: 220,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image(
+                            width: 100,
+                            image:
+                                AssetImage('assets/images/contact-logo.png')),
+                        this.contentStart()
+                      ],
+                    )))));
+  }
+
+  Widget contentStart() {
+    return FadeInUp(
+        from: 100,
+        child: Container(
+            child: Column(
+          children: [
+            Text("Downloading",
+                style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w300,
+                    fontFamily: 'ubuntu')),
+            SizedBox(height: 18),
+            CircularProgressIndicator(
+              strokeWidth: .7,
+            )
+          ],
+        )));
   }
 }
